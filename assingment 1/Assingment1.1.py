3aa0fbfaee0521732a90821410899531e6e8e225
Nikhil Upadhyay <maharajpanditji2@gmail.com>
2026-02-21 10:20:51 +0530

    Add files via upload

---
+++ assingment 1/Assingment1.1.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+print(" ")
+
+sum = num1+num2
+print("Addition:",sum)
+
+Diff = num1-num2
+print("Substraction:",Diff)
+
+Mul = num1*num2
+print("multiplication:",Mul)
+
+div = num1/num2
+print("Division:",div)
+
+
---
+++ assingment 1/Assingment1.2.py
@@ -0,0 +1,6 @@
+Name1 = str(input("Enter your first name: " ))
+Name2 = str(input("Enter your second name: "))
+
+Name3 = Name1+Name2+'!'
+print(" ")
+print("Hello,",Name3, 'Welcome to the python programm.')
